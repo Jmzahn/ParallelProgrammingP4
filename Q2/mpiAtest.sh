3f330c8cd88b76134f5f93429e2122cdc18f740b
Jacob Zahn <jmzahn@satx.rr.com>
2020-05-11 21:32:41 -0500

    edit test script

--- Q2/mpiAtest.sh
+++ Q2/mpiAtest.sh
@@ -21,24 +21,21 @@ module load intel openmpi
 echo "Testing partA.exe -np 1 ..."
 mpirun --machinefile machinefile.$JOB_ID -np 1 ./partA.exe 4
 echo -e "###\n"
-echo "Testing partA.exe -np 1 -npernode 2 ..."
-mpirun --machinefile machinefile.$JOB_ID -np 1 -npernode 2 ./partA.exe 4
+echo "Testing partA.exe -np 2 ..."
+mpirun --machinefile machinefile.$JOB_ID -np 2 ./partA.exe 4
 echo -e "###\n"
-echo "Testing partA.exe -np 2 -npernode 1 ..."
-mpirun --machinefile machinefile.$JOB_ID -np 2 -npernode 1 ./partA.exe 4
+echo "Testing partA.exe -np 4 ..."
+mpirun --machinefile machinefile.$JOB_ID -np 4 ./partA.exe 4
 echo -e "###\n"
-echo "Testing partA.exe -np 2 -npernode 2 ..."
-mpirun --machinefile machinefile.$JOB_ID -np 2 -npernode 2 ./partA.exe 4
+echo "Testing partA.exe -np 8 ..."
+mpirun --machinefile machinefile.$JOB_ID -np 8 ./partA.exe 8
 echo -e "###\n"
-echo "Testing partA.exe -np 2 -npernode 4 ..."
-mpirun --machinefile machinefile.$JOB_ID -np 2 -npernode 4 ./partA.exe 4
+echo "Testing partA.exe -np 16 ..."
+mpirun --machinefile machinefile.$JOB_ID -np 16 ./partA.exe 16
 echo -e "###\n"
-echo "Testing partA.exe -np 2 -npernode 8 ..."
-mpirun --machinefile machinefile.$JOB_ID -np 2 -npernode 8 ./partA.exe 4
+echo "Testing partA.exe -np 32 ..."
+mpirun --machinefile machinefile.$JOB_ID -np 32 ./partA.exe 32
 echo -e "###\n"
-echo "Testing partA.exe -np 2 -npernode 16 ..."
-mpirun --machinefile machinefile.$JOB_ID -np 2 -npernode 16 ./partA.exe 4
-echo -e "###\n"
-echo "Testing partA.exe -np 2 -npernode 32 ..."
-mpirun --machinefile machinefile.$JOB_ID -np 2 -npernode 32 ./partA.exe 4
+echo "Testing partA.exe -np 64 ..."
+mpirun --machinefile machinefile.$JOB_ID -np 64 ./partA.exe 64
 echo -e "###\n"
