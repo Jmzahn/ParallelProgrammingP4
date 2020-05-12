4829650e7278d8dc4a0925b1cc059c40cfd689e9
Jacob Zahn <jmzahn@satx.rr.com>
2020-05-12 00:58:54 -0500

    update B to last working ver of A

--- Q2/partB.c
+++ Q2/partB.c
@@ -1,117 +1,175 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define TAG 13
 
 int main(int argc, char *argv[]) {
-    double **A, **B, **C, *tmp;
+    double **A, *B, *X, *tmp, sum=0.0;
     double startTime, endTime;
     int numElements, offset, stripSize, myrank, numnodes, N, i, j, k;
-    
+    MPI_Status status;
     MPI_Init(&argc, &argv);
     
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     MPI_Comm_size(MPI_COMM_WORLD, &numnodes);
     
     N = atoi(argv[1]);
-
+    
+    int *workMap, *inttmp;
+    
     // allocate A, B, and C --- note that you want these to be
     // contiguously allocated.  Workers need less memory allocated.
 
-    if (myrank == 0) {
-        tmp = (double *) malloc (sizeof(double ) * N * N);
-        A = (double **) malloc (sizeof(double *) * N);
-        for (i = 0; i < N; i++)
-            A[i] = &tmp[i * N];
-    }
-    else {
-        tmp = (double *) malloc (sizeof(double ) * N * N / numnodes);
-        A = (double **) malloc (sizeof(double *) * N / numnodes);
-        for (i = 0; i < N / numnodes; i++)
-            A[i] = &tmp[i * N];
-    }
-    
-    
     tmp = (double *) malloc (sizeof(double ) * N * N);
-    B = (double **) malloc (sizeof(double *) * N);
+    A = (double **) malloc (sizeof(double *) * N);
     for (i = 0; i < N; i++)
-        B[i] = &tmp[i * N];
+        A[i] = &tmp[i * N];
     
-    
-    if (myrank == 0) {
-        tmp = (double *) malloc (sizeof(double ) * N * N);
-        C = (double **) malloc (sizeof(double *) * N);
-        for (i = 0; i < N; i++)
-            C[i] = &tmp[i * N];
-    }
-    else {
-        tmp = (double *) malloc (sizeof(double ) * N * N / numnodes);
-        C = (double **) malloc (sizeof(double *) * N / numnodes);
-        for (i = 0; i < N / numnodes; i++)
-            C[i] = &tmp[i * N];
-    }
+    tmp = (double *) malloc (sizeof(double ));
+    B = (double *) malloc (sizeof(double *) * N);
+    for (i = 0; i < N; i++)
+        B[i] = tmp[i];
+
+    inttmp = (int *) malloc (sizeof(int ));
+    workMap = (int *) malloc (sizeof(int *) * N);
+    for (i = 0; i < N; i++)
+        workMap[i] = inttmp[i];
 
     if (myrank == 0) {
+        tmp = (double *) malloc (sizeof(double ));
+        X = (double *) malloc (sizeof(double *) * N);
+        for (i = 0; i < N; i++)
+            X[i] = tmp[i];
+            X[i] = 0.0;
+    }
+    
+    if (myrank == 0) {
+        srand(time(NULL));
         // initialize A and B
         for (i=0; i<N; i++) {
             for (j=0; j<N; j++) {
-                A[i][j] = 1.0;
-                B[i][j] = 1.0;
+                A[i][j] = (float)rand() / 32768.0;
             }
+            B[i] = (float)rand() / 32768.0;
         }
     }
-    
+
+    stripSize = N/numnodes;
+    if(N % numnodes != 0){
+        MPI_Finalize();
+        if(myrank == 0){
+            printf("Input size is not divisible by No of nodes!\n");
+        }
+        exit(-1);
+    }
+    else{
+        if(myrank == 0){
+            printf("stripSize: %d\n",stripSize);
+        }
+    }
+
     // start timer
     if (myrank == 0) {
         startTime = MPI_Wtime();
     }
-    
-    stripSize = N/numnodes;
 
     // send each node its piece of A -- note could be done via MPI_Scatter
     if (myrank == 0) {
-        offset = stripSize;
-        numElements = stripSize * N;
+        printf("Starting initital communication.\n");
+        numElements = N * N;
         for (i=1; i<numnodes; i++) {
-            MPI_Send(A[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
-            offset += stripSize;
+            MPI_Send(&(A[0][0]), numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
         }
     }
     else {  // receive my part of A
-        MPI_Recv(A[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&(A[0][0]), N * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
     
     // everyone gets B
-    MPI_Bcast(B[0], N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    // Let each process initialize C to zero 
-    for (i=0; i<stripSize; i++) {
-        for (j=0; j<N; j++) {
-            C[i][j] = 0.0;
+    //MPI_Bcast(B[0], N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    if (myrank == 0){
+        numElements = N;
+        for (i=1; i<numnodes; i++) {
+            MPI_Send(&B[0], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
         }
     }
-
+    else{
+        MPI_Recv(&B[0], N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    if(myrank == 0){
+        printf("Done with initital communication.\nStarting work.\n");
+    }
+    
     // do the work
-    for (i=0; i<stripSize; i++) {
-        for (j=0; j<N; j++) {
-            for (k=0; k<N; k++) {
-            	C[i][j] += A[i][k] * B[k][j];
+    printf("%d: numnodes: %d\n",myrank,numnodes);
+    if(myrank == 0){
+        for(i = 0; i < N; i++){
+            workMap[i] = i % numnodes;
+        }
+        printf("Made workMap.\n");
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(&workMap[0], N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    double mult = 0.0;
+    for(k = 0; k < N-1; k++){
+        if(myrank == 0){
+            printf("Iteration %d\n",k);
+        }
+        printf("%d : k= %d, workMap[k]= %d\n",myrank,k,workMap[k]);
+        MPI_Bcast(&(A[k][k]), N-k, MPI_DOUBLE, workMap[k], MPI_COMM_WORLD);
+        MPI_Bcast(&B[k], 1, MPI_DOUBLE, workMap[k], MPI_COMM_WORLD);
+        for(i = k+1; i < N; i++){
+            if(workMap[i] == myrank){
+                mult = A[i][k]/A[k][k];
+                for(j = 0; j < N; j++){
+                    A[i][j] = A[i][j] - mult*A[k][j];
+                }
+                B[i] = B[i]- mult*B[k];
             }
         }
     }
 
-    // master receives from workers  -- note could be done via MPI_Gather
-    if (myrank == 0) {
-        offset = stripSize; 
-        numElements = stripSize * N;
+
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    // master receives A from workers  -- note could be done via MPI_Gather
+    if (myrank == 0){
+        printf("Starting final communication.\n");
+        numElements = N * N;
         for (i=1; i<numnodes; i++) {
-            MPI_Recv(C[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            offset += stripSize;
+            MPI_Recv(&A[0][0], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
     }
-    else { // send my contribution to C
-        MPI_Send(C[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
+    else { // send my contribution to A
+        MPI_Send(&A[0][0], N * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
+    }
+    //Send B
+    if (myrank == 0){
+        numElements = N;
+        for (i=1; i<numnodes; i++) {
+            MPI_Recv(&B[0], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+    }
+    else{
+        MPI_Send(&B[0], N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
+    }
+    if(myrank == 0){
+        printf("Done with final communication.\nStarting back prop.\n");
+    }
+
+    //master does back prop
+    if(myrank == 0){
+        X[N-1]=B[N-1]/A[N-1][N-1];
+        for(i = N-2; i >= 0; i--){
+            sum=0.0;
+            for(j = i+1; j < N; j++){
+                sum+=A[i][j]*X[j];
+            }
+            X[i] = (B[i]-sum)/A[i][i];
+        }
     }
 
     // stop timer
@@ -123,11 +181,9 @@ int main(int argc, char *argv[]) {
     // print out matrix here, if I'm the master
     if (myrank == 0 && N < 10) {
         for (i=0; i<N; i++) {
-            for (j=0; j<N; j++) {
-                printf("%f ", C[i][j]);
-            }
-            printf("\n");
+            printf("%f ", X[i]);
         }
+        printf("\n");
     }
     
     MPI_Finalize();
